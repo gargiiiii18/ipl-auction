1860c061714e0f8a097a8e048831964c865cd75f
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2024-10-02 23:11:23 +0530

    update PlayCard

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,32 +1,5 @@
 import React, {useEffect, useState} from 'react'
-const PlayerCard = ({auctionStarted, handleAuctionEnd}) => {
-
-  const[currentPlayer, setCurrentPlayer] = useState(null);
-
-  async function getCurrentPlayer() {
-    const url = "http://localhost:3000/currentplayer";
-    try {
-      const response = await fetch(url);
-      const jsonData = await response.json();
-      setCurrentPlayer(jsonData[0]);
-      const current_player_id = jsonData[0].player_id;
-      // console.log(current_player_id);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(()=>{
-    if(!auctionStarted){
-    getCurrentPlayer();
-    }
-  }, [auctionStarted]);
-
- const endAuction = () => { //whenever end auction logic is written, write it in endAuction func
-  handleAuctionEnd();
-  setCurrentPlayer(null);
- }
-
+const PlayerCard = ({auctionStarted, onAuctionEnd, currentPlayer}) => {
 
   return (
     <div>
