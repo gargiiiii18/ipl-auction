ba870c38502638e20ddbaecec681b7e13cffd74a
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2024-10-02 23:11:15 +0530

    update Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,8 +7,33 @@ import PlayerCard from './PlayerCard';
 const Homepage = () => {
 
   const[isClicked, setClicked] = useState(false);
-  const[auctionStarted, setAuctionStarted] = useState(false);
+  const[auctionStarted, setAuctionStarted] = useState(true);
   const[buttonText, setButtonText] = useState("Add Team");
+  const[currentPlayer, setCurrentPlayer] = useState(null);
+
+
+  const onAuctionEnd = async () => {
+    // console.log("i have been summoned");
+    
+    try {
+      const url = "http://localhost:3000/endauction"
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if(!response.ok){
+        throw new Error("Failed to end Auction");
+      }
+
+      const result = response.json();
+      console.log(result.message);
+      setAuctionStarted(false);
+    } catch (error) {
+      console.log(error); 
+    }
+  }
 
   useEffect(()=>{
     const checkAuctionStatus = async () =>{
@@ -24,19 +49,31 @@ const Homepage = () => {
     };
     checkAuctionStatus();
   }, [])
-  
+
+  async function getCurrentPlayer() {
+    const url = "http://localhost:3000/currentplayer";
+    try {
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      setCurrentPlayer(jsonData[0]);
+      // console.log(jsonData[0].player_name);
+      
+      const current_player_id = jsonData[0].player_id;
+      // console.log(current_player_id);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    if(!auctionStarted){
+    getCurrentPlayer();
+    }
+  }, [auctionStarted]);
+
   const handleClick = () => {
     setClicked(!isClicked);
    setButtonText(isClicked?'Add Team':'Close')
-  }
-
-  const handleAuctionStart = () => {
-    setAuctionStarted(true);
-  }
-
-  const handleAuctionEnd = () => {
-    // onAuctionEnd();
-    setCurrentPlayer(null);
   }
 
   return (
@@ -45,15 +82,15 @@ const Homepage = () => {
      <h1>IPL Auction</h1>
      </div>
      <div className="teamContainer">
-    <TeamCard/>
+    <TeamCard currentPlayer={currentPlayer}/>
     <button onClick={handleClick} className='addTeamBtn'>{buttonText}</button>
     {isClicked &&
     <AddTeam/>
 }
   {!isClicked && !auctionStarted &&
-    <button className='startAuctionBtn' onClick={handleAuctionStart}>Start Auction</button>
+    <button className='endAuctionBtn' onClick={onAuctionEnd}>Get Player</button>
   }
-    <PlayerCard/>
+    <PlayerCard currentPlayer={currentPlayer} auctionStarted={auctionStarted} />
     </div>
     </Fragment>
   )
