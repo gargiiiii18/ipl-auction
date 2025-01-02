2f61c5d28f713ba167fb0d6735da5f0629ee94e5
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-01-02 21:27:03 +0530

    updated

--- src/components/AddBid.jsx
+++ src/components/AddBid.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-const AddBid = ({team_id, onBidResponse, message}) => {
+const AddBid = ({showBid, onBidResponse, message}) => {
 
 const [bid, setBid]  = useState(""); 
 
 const navigate = useNavigate();
-// const team_id = useParams();
+const team_id_obj = useParams();
+const team_id = team_id_obj.team_id;
 
 const handleChange = (event) => {
     setBid(event.target.value);
@@ -16,6 +17,8 @@ const handleSubmit = async (event) => {
   event.preventDefault();
   console.log(bid);
   const url = `http://localhost:3000/teams/${team_id}`
+  // console.log(team_id);
+  
   try {
     const body = {bid};
     const response = await fetch(url, {
@@ -25,13 +28,22 @@ const handleSubmit = async (event) => {
     });
 
     const data = await response.json();
-    if(data.status==="success"){
-      console.log("redirecting..."); 
-      onBidResponse("success", "Bid successfully added.");
-      navigate(`/teams/${team_id}`);
-    }
-    else if(data.status==="error"){
+    // if(data.status==="success"){
+    //   console.log("redirecting..."); 
+    //   onBidResponse("success", message);
+    
+    //   navigate(`/teams/${team_id}`);
+    //   setBid("");
+    // }
+    if(data.status==="error"){
      onBidResponse("error", data.message || "Failed to add bid.");
+    }
+    else if(data.status==="success"){
+      console.log("redirecting..."); 
+      onBidResponse("success", message);
+    
+      navigate(`/teams/${team_id}`);
+      setBid("");
     }
   } catch (error) {
     console.log(error);
@@ -47,7 +59,7 @@ const handleSubmit = async (event) => {
        }
        <form className='bidForm' action="">
         <label htmlFor="bid">Bid</label>
-        <input type='text' value={bid} onChange={handleChange}/>
+        <input type='number' value={bid} onChange={handleChange}/>
         <button className='submitBid' onClick={handleSubmit}>Done</button>
        </form>
     </div>
