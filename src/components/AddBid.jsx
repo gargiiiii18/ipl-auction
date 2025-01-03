cb8ef89ff3d17516c4009e3e2244afd00dd12853
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-01-03 14:28:19 +0530

    updated

--- src/components/AddBid.jsx
+++ src/components/AddBid.jsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 const AddBid = ({showBid, onBidResponse, message}) => {
 
 const [bid, setBid]  = useState(""); 
+const [enoughBudgetMsg, setEnoughBudgetMsg] = useState("");
 
 const navigate = useNavigate();
 const team_id_obj = useParams();
@@ -35,13 +36,16 @@ const handleSubmit = async (event) => {
     //   navigate(`/teams/${team_id}`);
     //   setBid("");
     // }
+    
     if(data.status==="error"){
      onBidResponse("error", data.message || "Failed to add bid.");
+     setEnoughBudgetMsg(data.message);
+     setBid("");
     }
     else if(data.status==="success"){
       console.log("redirecting..."); 
-      onBidResponse("success", message);
-    
+      onBidResponse("success", data.message);
+      setEnoughBudgetMsg(data.message);
       navigate(`/teams/${team_id}`);
       setBid("");
     }
@@ -54,9 +58,7 @@ const handleSubmit = async (event) => {
   return (
     <div>
        {/* <h2>Current Player: {currentPlayer}</h2> */}
-       {message && 
-       <h2>{message}</h2>
-       }
+       <h2>{enoughBudgetMsg}</h2>
        <form className='bidForm' action="">
         <label htmlFor="bid">Bid</label>
         <input type='number' value={bid} onChange={handleChange}/>
