3f2ea0ab04d8d404560394a35c8db0afc8881156
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-01-05 18:19:18 +0530

    created

---
+++ src/components/DisplayResult.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DisplayResult = () => {
+  return (
+    <div className='resultContainer'>
+      <h2>Hello</h2>
+    </div>
+  )
+}
+
+export default DisplayResult
