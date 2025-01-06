109e55cb8fa741e87403affbd18c19e2d7cfb5f2
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-01-06 21:26:54 +0530

    updated

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -5,7 +5,10 @@ const PlayerCard = ({auctionStarted, onAuctionEnd, currentPlayer}) => {
     <div>
            {
             currentPlayer ? 
+            <div>
           <h1 className="player">Current Player: {currentPlayer.player_name}</h1> 
+          <h2 className="playerPrice">Price: {currentPlayer.price}</h2> 
+          </div>
           : 
         <h1 className="player">No Players to Bid.</h1>
       } 
