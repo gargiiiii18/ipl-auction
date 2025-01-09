80e7fad8b80b47e24aa748ccaccc129c77fe2f6b
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-01-09 20:28:34 +0530

    final commit

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -5,9 +5,13 @@ const PlayerCard = ({auctionStarted, onAuctionEnd, currentPlayer}) => {
     <div>
            {
             currentPlayer ? 
-            <div>
-          <h1 className="player">Current Player: {currentPlayer.player_name}</h1> 
-          <h2 className="playerPrice">Price: {currentPlayer.price}</h2> 
+            <div className='playerInfo'>
+          <h1 className="player">Current Player</h1> 
+          <h1 className="playerName">{currentPlayer.player_name}</h1> 
+          <div className="playerDetails">
+          <h2 className="playerPrice">Price: {currentPlayer.price}cr</h2> 
+          <h2 className="playerPrice">Skills: {currentPlayer.skills}</h2> 
+          </div>
           </div>
           : 
         <h1 className="player">No Players to Bid.</h1>
